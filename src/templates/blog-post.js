087cdbec6bf90a9d450f4b6e25a2b6cc60ff651e
Victor Oliveira <vamo89@gmail.com>
2019-05-09 21:38:32 -0300

    last http to https

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -25,7 +25,7 @@ export const BlogPostTemplate = ({
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
               {title}
             </h1>
-            <p><img src={`http://img.youtube.com/vi/${ytkey}/maxresdefault.jpg`} alt="Youtube thumbnail"/></p>
+            <p><img src={`https://img.youtube.com/vi/${ytkey}/maxresdefault.jpg`} alt="Youtube thumbnail"/></p>
             <PostContent content={content} />
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
