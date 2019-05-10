4301540e60ded6bd07943b76a0f230c8c80d2df3
Victor Oliveira <vamo89@gmail.com>
2019-05-09 22:55:16 -0300

    blogroll 3 por linha - remove não usado features

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -11,7 +11,7 @@ class BlogRoll extends React.Component {
       <div className="columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
+            <div className="is-parent column is-4" key={post.id}>
               <article
                 className={`blog-list-item tile is-child box notification`}
               >
